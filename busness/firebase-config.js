81f0cb7d3d0ca04ea041df780071331c57ae827a
parameshwaran <139535076+Paramesh1suji2006@users.noreply.github.com>
2026-09-06 22:41:13 +0530

    Delete busness directory

--- busness/firebase-config.js
+++
@@ -1,9 +0,0 @@
-// Your Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAljsGyJVhirRs07ldWL7UiQXu27EpmUVw",
-  authDomain: "tea-ledger-2a7a8.firebaseapp.com",
-  projectId: "tea-ledger-2a7a8",
-  storageBucket: "tea-ledger-2a7a8.firebasestorage.app",
-  messagingSenderId: "934367790111",
-  appId: "1:934367790111:web:d2bf0775441ffc38cb012a"
-};
